6c0b293fa943c46ac44a27f6a2ae3a57be7fa103
Shashi Bhagat <skujur871@gmail.com>
2021-08-28 03:37:20 +0530

    Implemented redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log("Hello World!");
+import { Map } from 'immutable';
+import { produce } from 'immer';
+
+import store from './store';
+
+// let book = Map({ title: "Harry Potter" });
+let book = { title: "Harry Potter" };
+
+
+function publish(book) {
+    return produce(book, draftBook => {
+        draftBook.isPublished = true;
+    })
+    // book.isPublished = true;
+}
+
+
+const updatedBook = publish(book);
+
+console.log(updatedBook);
+
+// Immer ==> Library
+
+const unSubscribe = store.subscribe(() => {
+    console.log("Store change!", store.getState())
+})
+
+store.dispatch({
+    type: 'bugAdded',
+    payload: {
+        description: 'funny bug'
+    }
+});
+
+unSubscribe();
+
+store.dispatch({
+    type: 'bugRemoved',
+    payload: {
+        id: 1
+    }
+})
+
+console.log(store.getState());
