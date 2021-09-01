08d40964394da82802d0fed8da7a765d493a9cff
Shashi Bhagat <skujur871@gmail.com>
2021-09-02 01:35:40 +0530

    middleware default

--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -2,6 +2,7 @@ const logger = store => next => action => {
     console.log('store', store)
     console.log('next', next)
     console.log('action', action)
+    next(action)
 }
 
 export default logger;
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import reducer from './reducers';
 import logger from './middleware/logger'
 
 export default configureStore({
     reducer,
-    middleware: [logger]
+    middleware: [...getDefaultMiddleware(), logger]
 })
